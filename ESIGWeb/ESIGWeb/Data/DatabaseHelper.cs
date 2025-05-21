56adae68e6727f63af2ae85e5631098cc847ebd1
Gabriel <gapima7@gmail.com>
2025-05-21 19:08:20 -0300

    start project

---
+++ ESIGWeb/ESIGWeb/Data/DatabaseHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ESIGWeb.Data
+{
+    public static class DatabaseHelper
+    {
+        // Lê a connection string do Web.config
+        private static readonly string ConnectionString =
+            ConfigurationManager
+                .ConnectionStrings["OracleConnection"]
+                .ConnectionString;
+
+        /// <summary>
+        /// Executa a procedure SP_CALCULAR_SALARIOS no Oracle,
+        /// que preenche ou atualiza a tabela pessoa_salario.
+        /// </summary>
+        public static void ExecutarProcedureCalculo()
+        {
+            using (var conn = new OracleConnection(ConnectionString))
+            {
+                conn.Open();
+                using (var cmd = new OracleCommand("calcular_salarios", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Retorna todos os registros da tabela pessoa_salario,
+        /// já ordenados por pessoa_id, prontos para exibir no GridView.
+        /// </summary>
+        public static DataTable ObterPessoasSalarios()
+        {
+            var dt = new DataTable();
+
+            using (var conn = new OracleConnection(ConnectionString))
+            {
+                conn.Open();
+
+                const string sql = @"
+                    SELECT
+                        pessoa_id,
+                        nome,
+                        salario_bruto,
+                        descontos,
+                        salario_liquido
+                    FROM pessoa_salario
+                    ORDER BY pessoa_id";
+
+                using (var cmd = new OracleCommand(sql, conn))
+                using (var adapter = new OracleDataAdapter(cmd))
+                {
+                    adapter.Fill(dt);
+                }
+            }
+
+            return dt;
+        }
+    }
+}
---
+++ ESIGWeb/ESIGWeb/Listagem.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using ESIGWeb.Data; // namespace onde está DatabaseHelper
+
+namespace ESIGWeb
+{
+    public partial class Listagem : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                CarregarDados();
+        }
+
+        private void CarregarDados()
+        {
+            DataTable dt = DatabaseHelper.ObterPessoasSalarios();
+            gridPessoas.DataSource = dt;
+            gridPessoas.DataBind();
+        }
+
+        protected void btnCalcular_Click(object sender, EventArgs e)
+        {
+            DatabaseHelper.ExecutarProcedureCalculo();
+            CarregarDados();
+        }
+    }
+}
